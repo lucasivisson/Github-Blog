ffd3668ae5cd8b88b0947b895fc57e7cdd73e70d
Lucas Ivisson <lucasivisson1@gmail.com>
2023-04-17 14:51:37 -0300

    📝 🔧 introducing styled-components on global and default style

---
+++ src/styles/global.ts
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  a {
+    color: ${props => props.theme.blue}
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  input {
+    background: ${props => props.theme['base-input']};
+    color: ${props => props.theme['base-label']};
+    border: 1px solid ${props => props.theme['base-border']};
+  }
+
+  input:focus {
+    outline: 0;
+    border: 1px solid ${props => props.theme['blue']};
+    color: ${props => props.theme['base-subtitle']};
+  }
+
+  body {
+    background-color: ${props => props.theme['base-background']};
+    color: ${props => props.theme['base-text']};
+    line-height: 160%;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font: 400 1rem Nunito, sans-serif;
+  }
+`
