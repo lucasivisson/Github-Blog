dade9348fbdeca6e1b3c915b2ab85a8110b02100
Lucas Ivisson <lucasivisson1@gmail.com>
2023-04-21 13:47:58 -0300

    ♻️ refactor router and remove console.log

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,24 +3,11 @@ import { DefaultLayout } from './layouts/defaultLayout';
 import { Home } from './pages/Home';
 import { Post } from './pages/Post';
 
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <DefaultLayout/>,
-//     children: [
-//       {
-//         path: '/',
-//         element: <h1>Issue</h1>
-//       }
-//     ]
-//   }
-// ]);
-
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<DefaultLayout/>}>
       <Route path="" element={<Home/>}></Route>
-      <Route path="post/:postId" element={<Post/>}></Route>
+      <Route path="issue/:issueId" element={<Post/>}></Route>
     </Route>
   )
 )
