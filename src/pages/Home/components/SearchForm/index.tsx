616164f384347d28d69b37809df8c6871d409fe1
Lucas Ivisson <lucasivisson1@gmail.com>
2023-04-24 13:02:45 -0300

    🚧 working with react hook form

--- src/pages/Home/components/SearchForm/index.tsx
+++ src/pages/Home/components/SearchForm/index.tsx
@@ -1,14 +1,46 @@
+import { useEffect, useState } from "react";
 import { SearchFormContainer } from "./styles";
+import { useForm } from 'react-hook-form';
 
 export function SearchForm() {
+  const { register, handleSubmit, watch } = useForm();
+  const [stringToSearch, setStringToSearch] = useState<string>('');
+  const searchString = watch('search');
+
+  function onSubmit(data: any) {
+    console.log(data);
+  }
+
+  function handleNewStringToSearchChange() {
+    console.log(searchString);
+    setStringToSearch(searchString);
+    console.log(stringToSearch);
+  }
+
+  // useEffect(() => { 
+  //   let isCancelled = false; 
+  //   const changeHandler = async () => { 
+  //   await timeout(1000); 
+  //   if (!isCancelled) { 
+  //   alert(`A name was changed: ${stringToSearch}`); 
+  //   } 
+  //   }; 
+    
+  //   changeHandler(); 
+  //   //The cleanup function is called when useEffect is called again or on unmount. 
+  //   return () => { 
+  //   isCancelled = true; 
+  //   }; 
+  //  }, [stringToSearch]); 
+
   return (
     <SearchFormContainer>
       <div>
         <span>Publicações</span>
         <span>6 publicações</span>
       </div>
-      <form action="">
-        <input type="text" placeholder='Buscar conteúdo'/>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" placeholder='Buscar conteúdo' {...register("search")} onChange={handleNewStringToSearchChange}/>
       </form>
     </SearchFormContainer>
   );
